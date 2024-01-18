332920c1645b24f96942ab0a54a4241c352190a5
ephrim-s <ephrims5@gmail.com>
2024-01-18 13:43:41 +0000

    import and modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#! __name__ == "__main__":
+"""return the result of different operation"""
+from calculator_1 import add, sub, mult, div
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, add(a, b)))
+print("{} * {} = {}".format(a, b, add(a, b)))
+print("{} / {} = {}".format(a, b, add(a, b)))
