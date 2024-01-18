c3ea47533c08bbdd6dc5a7aaf8901e2fc38c6a70
ephrim-s <ephrims5@gmail.com>
2024-01-18 06:18:40 -0800

    update

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """print names of hidden module"""
+
+    import hidden_4
+
+    lst = dir(hidden_4)
+    for name in lst:
+        if nam[:2] != "__":
+            print(name)
